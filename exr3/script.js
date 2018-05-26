f81b9caadd0f124f9e6e4d061bc36d0300e4dfc0
Jackob <abdurashidov.aak@students.dvfu.ru>
2018-05-27 01:32:38 +1000

    Task 3.2

--- exr3/script.js
+++ exr3/script.js
@@ -8,3 +8,23 @@ function CalculateSquare() {
       Source.Result.value=a*b;
     }
 }
+
+function SumSer() {
+  var base=document.getElementById('independentX').value;
+  var s=parseFloat(base);
+  var i=3;
+  var eps=0.005;
+  if (s>=1) {
+    return;
+  }
+  while (((Math.pow(base, i) / i) > eps)) {
+    s+=(Math.pow(base, i) / i);
+    i+=2;
+  }
+
+  s*=10000000;
+  s=Math.round(s);
+  s/=10000000;
+
+  document.getElementById('Result').innerHTML="S="+s;
+}
